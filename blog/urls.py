9aaf5c0a8ae2b4a921b1804a021817c84e263745
Mariolini <mh4gh@himaus.de>
2022-03-13 11:34:49 +0100

    all Pages

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from struct import pack
 from django.urls import path
-from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView
+from .views import BlogListView, BlogDetailView, BlogCreateView, BlogUpdateView, BlockDeleteView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="home"),
     path("post/<int:pk>/", BlogDetailView.as_view(), name="post_detail"),
     path("post/new/", BlogCreateView.as_view(), name="post_new"),
     path("post/<int:pk>/edit/", BlogUpdateView.as_view(), name="post_edit"),
+    path("post/<int:pk>/delete/", BlockDeleteView.as_view(), name="post_delete"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from pyexpat import model
-from re import template
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 from .models import Post
 
@@ -22,3 +21,8 @@ class BlogUpdateView(UpdateView):
     model = Post
     template_name = "post_edit.html"
     fields = ["title", "body"]
+
+class BlockDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
